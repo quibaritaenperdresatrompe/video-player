ca9134fe30952ead63071ea72c7cc9af8673c0db
Valentin Barit <valentin.barit@gmail.com>
2017-12-29 18:29:15 +1000

    feat(Icons): add Volume icons

---
+++ src/components/VolumeUpIcon/__tests__/index.js
@@ -0,0 +1,22 @@
+import VolumeUpIcon from '../'
+
+import itRendersAllMutations from '../../../utils/jest-it-renders-all-mutations'
+
+const mutations = [
+  {
+    name: 'without props',
+    props: null,
+  },
+  {
+    name: 'with all props',
+    props: {
+      backgroundColor: '#000',
+      color: '#fff',
+      size: '3em',
+    },
+  },
+]
+
+describe(VolumeUpIcon.name, () => {
+  itRendersAllMutations(VolumeUpIcon, mutations)
+})
