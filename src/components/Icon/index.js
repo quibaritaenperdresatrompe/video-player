8f0beec396167c51a7256ff8cb9de1716c5698c0
Valentin Barit <valentin.barit@gmail.com>
2017-12-30 15:31:00 +1000

    feat(Icon): add Fullscreen icons

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -10,6 +10,8 @@ const StyledIcon = glamorous.svg(props => ({
 }))
 
 const ICON_PATHS = {
+  FullscreenIcon: 'M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z',
+  FullscreenExitIcon: 'M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z',
   PauseIcon: 'M6 19h4V5H6v14zm8-14v14h4V5h-4z',
   PlayIcon: 'M8 5v14l11-7z',
   ReplayIcon: 'M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z',
